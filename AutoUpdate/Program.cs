06bc193f4dafeca1d9c7bfc02f3f55a60e0bc318
Trần Mạnh <manhtran98bg@gmail.com>
2025-07-23 12:28:19 +0700

    commit

--- AutoUpdate/Program.cs
+++ AutoUpdate/Program.cs
@@ -1,16 +1,21 @@
 ﻿using Avalonia;
 using System;
+using System.Runtime.InteropServices;
 using Squirrel;
 namespace AutoUpdate;
 
 sealed class Program
 {
+    [DllImport("kernel32.dll")]
+    private static extern bool AllocConsole();
     // Initialization code. Don't use any Avalonia, third-party APIs or any
     // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
     // yet and stuff might break.
     [STAThread]
     public static void Main(string[] args)
     {
+        AllocConsole();
+        Console.WriteLine("main app running");
         BuildAvaloniaApp()
             .StartWithClassicDesktopLifetime(args);
     }
